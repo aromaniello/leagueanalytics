c530ce702c6386a928e2b17a5aba7ea3a0738a74
Agustin Romaniello <agustin@romaniello.com.ar>
2021-05-24 11:40:17 -0300

    Install and configure react

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module Leagueanalytics
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.react.server_renderer_extensions = ["jsx", "js", "tsx", "ts"]
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
